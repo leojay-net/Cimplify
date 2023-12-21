dfffe08e6c16db645b188b74665c7a6f7c7ecbff
Joseph Aleonomoh <102752992+leojay-net@users.noreply.github.com>
2023-12-21 14:43:04 +0100

    Update serializers.py

--- chat/serializers.py
+++ chat/serializers.py
@@ -4,5 +4,5 @@ from rest_framework import serializers
 
 class ChatSerializer(serializers.Serializer):
     problem = serializers.CharField(max_length=300)
-    aoc = serializers.CharField(max_length=300)
+    aoc = serializers.CharField(max_length=300, required=False)
     
